476d6cb8cda7952afc9ebed49d39e5deedf6eac1
Sopel97 <ts.tomeksopel@gmail.com>
2017-07-14 19:02:50 +0200

    added SpriteBatch class that will be used for rendering

---
+++ include/SpriteBatch.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <unordered_map>
+#include <vector>
+
+#include <SFML/Graphics.hpp>
+
+#include "../LibS/Shapes.h"
+
+class SpriteBatch : public sf::Drawable
+{
+public:
+    SpriteBatch();
+
+    void emplaceRectangle(const sf::Texture* texture, const ls::Vec2F& position, const ls::Vec2F& texCoords, const ls::Vec2F& size, const sf::Color& color = sf::Color::White);
+
+    void clear();
+
+private:
+    static constexpr int m_defaultNumBuffers = 32;
+    static constexpr int m_defaultBufferSize = 256;
+
+    void draw(sf::RenderTarget& renderTarget, sf::RenderStates renderStates) const override;
+
+    unsigned textureHandle(const sf::Texture* texture) const;
+
+    std::vector<sf::Vertex>& createBuffer(const sf::Texture* texture);
+
+    std::vector<sf::Vertex>& findBuffer(const sf::Texture* texture);
+
+    std::unordered_map<unsigned, std::vector<sf::Vertex>> m_vertexBuffersByTexture;
+};
---
+++ src/SpriteBatch.cpp
@@ -0,0 +1,66 @@
+#include "SpriteBatch.h"
+
+
+SpriteBatch::SpriteBatch() :
+    m_vertexBuffersByTexture(m_defaultNumBuffers)
+{
+
+}
+
+void SpriteBatch::emplaceRectangle(const sf::Texture* texture, const ls::Vec2F& position, const ls::Vec2F& texCoords, const ls::Vec2F& size, const sf::Color& color)
+{
+    const sf::Vertex topLeft(sf::Vector2f(position.x, position.y), color, sf::Vector2f(texCoords.x, texCoords.y));
+    const sf::Vertex topRight(sf::Vector2f(position.x + size.x, position.y), color, sf::Vector2f(texCoords.x + size.x, texCoords.y));
+    const sf::Vertex bottomLeft(sf::Vector2f(position.x, position.y + size.y), color, sf::Vector2f(texCoords.x, texCoords.y + size.y));
+    const sf::Vertex bottomRight(sf::Vector2f(position.x + size.x, position.y + size.y), color, sf::Vector2f(texCoords.x + size.x, texCoords.y + size.y));
+
+    auto& buffer = findBuffer(texture);
+
+    buffer.emplace_back(topLeft);
+    buffer.emplace_back(topRight);
+    buffer.emplace_back(bottomRight);
+
+    buffer.emplace_back(topLeft);
+    buffer.emplace_back(bottomRight);
+    buffer.emplace_back(bottomLeft);
+}
+
+void SpriteBatch::clear()
+{
+    m_vertexBuffersByTexture.clear();
+}
+void SpriteBatch::draw(sf::RenderTarget& renderTarget, sf::RenderStates renderStates) const
+{
+    for (auto& p : m_vertexBuffersByTexture)
+    {
+        auto& buffer = p.second;
+
+        if (buffer.size() == 0) continue;
+        renderTarget.draw(buffer.data(), buffer.size(), sf::PrimitiveType::Triangles, renderStates);
+    }
+}
+
+unsigned SpriteBatch::textureHandle(const sf::Texture* texture) const
+{
+    if (texture == nullptr) return 0u;
+    
+    return texture->getNativeHandle();
+}
+std::vector<sf::Vertex>& SpriteBatch::createBuffer(const sf::Texture* texture)
+{
+    const unsigned handle = textureHandle(texture);
+    auto iter = m_vertexBuffersByTexture.emplace(handle, std::vector<sf::Vertex>{});
+
+    std::vector<sf::Vertex>& buffer = iter.first->second;
+    buffer.reserve(m_defaultBufferSize);
+    return buffer;
+}
+
+std::vector<sf::Vertex>& SpriteBatch::findBuffer(const sf::Texture* texture)
+{
+    const unsigned handle = textureHandle(texture);
+    auto iter = m_vertexBuffersByTexture.find(handle);
+    if (iter == m_vertexBuffersByTexture.end()) return createBuffer(texture);
+
+    return iter->second;
+}
